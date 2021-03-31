9237ea2fc5eb4d38de667de69df9c6aaf954e648
thottunkan <ambadinarayan98@gmail.com>
2021-03-31 15:21:17 +0530

    about us half complete

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -12,5 +12,48 @@
     </div>
     
 </div>
+<div class="container">
+    <div class="row">
+        <div class="col col-md-3 col-sm-3 col-lg-3 col-xs-3">
+            <table class="table table-borderless">
+                <tr>
+                    <td>ABOUT THE INSTITUTION</td>
+                </tr>
+                <tr>
+                    <td>VISON AND  MISSION</td>
+                </tr>
+                <tr>
+                    <td>ADMINISTRATION</td>
+                </tr>
+                <tr>
+                    <td>COMMETITES</td>
+                </tr>
+                <tr>
+                    <td>ORGANOGRAM</td>
+                </tr>
+                <tr>
+                    <td>RANKING</td>
+                </tr>
+                <tr>
+                    <td>ACCREDIATIONS</td>
+                </tr>
+                <tr>
+                    <td>POLICY DOCUMENTS</td>
+                </tr>
+            </table>
+        </div>
+        <div class="col col-md-8 col-lg-8 ">
+            <h1>About the Institution</h1>
+            <div class="row">
+                <div class="col-4">
+                      <img src="https://rajagiri.edu/uploads/about-instituition/1/1.jpg" class="img-fluid" alt="">  
+                </div>
+                <div class="col-8">
+                    <p>Rajagiri College of Social Sciences (RCSS), together with its sister educational concerns, owes its existence to the CMI (Carmelites of Mary Immaculate) fathers, the first ever indigenous religious congregation for men in the Syrian Catholic tradition of Christianity in India. The CMIs drawing inspiration from their founding father St. Kuriakose Elias Chavara, a great visionary, reformer and religious leader of the 19th century, have proven themselves worthy of that heritage in the field of education by establishing institutions of excellence imparting quality education, across the length and breadth of the State, and in various parts of India. At present, the CMIs manage a network of more than 800 Schools, 13 Special Schools, 41 Arts, Science, Commerce & Physical Education Colleges, 10 B.Ed. Colleges, 4 Nursing Colleges, 4 Engineering Colleges, one Medical College, one University, 3 Law Colleges, 6 Research Centres, 7 Technical Institutes, 41 Hostels & Boarding Houses, 17 Cultural Centres, 2 Super Speciality Hospitals and 114 Non-formal Education Centres.  </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
